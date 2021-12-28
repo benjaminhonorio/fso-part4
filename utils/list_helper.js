5bfd01b4f528030e0e3b9dae2c8a44ef6513d405
benjaminhonorio <benjaminhonorio.12@gmail.com>
2021-12-27 23:47:09 -0500

    Helper functions and unit test step3 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,4 +8,15 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlogs = (blogs) => {
+  const favorite = blogs.reduce((mostLiked, blog) => {
+    return mostLiked.likes >= blog.likes ? mostLiked : blog;
+  });
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
+module.exports = { dummy, totalLikes, favoriteBlogs };
