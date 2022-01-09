3e19f04decf2f5576a86faaaad7780b4c1c48049
benjaminhonorio <benjaminhonorio.12@gmail.com>
2022-01-09 15:05:58 -0500

    Add update and delete routes

--- controller/blogs.js
+++ controller/blogs.js
@@ -16,4 +16,21 @@ blogsRouter.post("/", async (request, response) => {
   response.status(201).json(result);
 });
 
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+  const blog = {
+    ...body.blog,
+    likes: body.likes,
+  };
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
+
 module.exports = blogsRouter;
